c7b672035b6a43b262d5c5e2f71c336476fa43b1
rikbrouwer <63099707+rikbrouwer@users.noreply.github.com>
2020-06-01 14:37:31 +0200

    Add files via upload

--- src/data-preparation/parse.py
+++ src/data-preparation/parse.py
@@ -6,7 +6,7 @@ con = f.readlines()
 
 outfile = open('../../gen/data-preparation/temp/parsed-data.csv', 'w', encoding = 'utf-8')
 
-outfile.write('id\tcreated_at\ttext\n')
+outfile.write('id\ttext\tsource\n')
 
 cnt = 0
 for line in con:
@@ -15,9 +15,11 @@ for line in con:
     cnt+=1
     obj = json.loads(line.replace('\n',''))
 
+
+    device = obj.get('source')
     text = obj.get('text')
     text = text.replace('\t', '').replace('\n', '')
 
-    outfile.write(obj.get('id_str')+'\t'+obj.get('created_at')+'\t'+text+'\n')
+    outfile.write(obj.get('id_str')+'\t'+text+'\t'+device+'\n')
 
 print('done.')
